b3145db6f36b09897caffb5a054cad2afbdb7e6e
Nantia Leonidou <nantia.leonidou@student.uni-tuebingen.de>
2018-05-19 15:54:07 +0200

    delete irrelevant code

--- table/table.cpp
+++ table/table.cpp
@@ -9,24 +9,6 @@ Table
 {
     ui->setupUi(this);
 
-    // Create a new model
-    // QStandardItemModel(int rows, int columns, QObject * parent = 0)
-    model = new QStandardItemModel(4,2,this);
-
-    // Attach the model to the view
-    //ui->tableView->setModel(model);
-
-    // Generate data
-    for(int row = 0; row < 4; row++)
-    {
-        for(int col = 0; col < 2; col++)
-        {
-            QModelIndex index
-                    = model->index(row,col,QModelIndex());
-            // 0 for all data
-            model->setData(index,0);
-        }
-    }
 }
 
 
@@ -34,3 +16,5 @@ Table::~Table()
 {
     delete ui;
 }
+
+
