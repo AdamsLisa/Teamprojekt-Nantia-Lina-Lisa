14c324fefdd88375139c3ff731bd3f3ef714ade9
Nantia Leonidou <nantia.leonidou@student.uni-tuebingen.de>
2018-05-29 15:52:11 +0200

    Delegate implementation

---
+++ QTableViewTabelle/progressdelegate.h
@@ -0,0 +1,26 @@
+#ifndef PROGSSDELEGATE_H
+
+#include <QStyledItemDelegate>
+
+class ProgressDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+
+public:
+    ProgressDelegate(QWidget *parent = 0) : QStyledItemDelegate(parent) {}
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &option,
+               const QModelIndex &index) const override;
+    QSize sizeHint(const QStyleOptionViewItem &option,
+                   const QModelIndex &index) const override;
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
+                          const QModelIndex &index) const override;
+    void setEditorData(QWidget *editor, const QModelIndex &index) const override;
+    void setModelData(QWidget *editor, QAbstractItemModel *model,
+                      const QModelIndex &index) const override;
+
+private slots:
+    void commitAndCloseEditor();
+};
+
+#endif
