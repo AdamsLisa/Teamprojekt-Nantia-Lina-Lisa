b9c4c64adf186399d69232bd66ec723ef8cf23e6
AdamsLisa <39195302+AdamsLisa@users.noreply.github.com>
2018-07-17 18:01:15 +0200

    Bei Filtern der Proteintabelle werden direkt passende Peptide angezeigt

--- QTableViewTabelle/bardelegatepep.cpp
+++ QTableViewTabelle/bardelegatepep.cpp
@@ -27,15 +27,19 @@ void bardelegatepep::paint(QPainter *painter, const QStyleOptionViewItem & optio
     //convert data to string
     QString dataToString = index.data().toString();
 
-    //da Qt anscheinend alle QStandarditems zu float konvertieren kann, hier nun die Abfrage, ob wir tatsächlich eine Zahl vorliegen haben
-    if (dataToString.startsWith("0") || dataToString.startsWith("1") || dataToString.startsWith("2") || dataToString.startsWith("3")
-            || dataToString.startsWith("4") || dataToString.startsWith("5") || dataToString.startsWith("6") || dataToString.startsWith("7")
-            || dataToString.startsWith("8") || dataToString.startsWith("9")){
+
+    //If Bedingung, damit nur bestimmte Spalten Balken anzeigen
+    if (((index.model()->headerData(index.column(),Qt::Horizontal)).toString() == "Confidence") ||
+        ((index.model()->headerData(index.column(),Qt::Horizontal)).toString() == "# Spectra"))
+{
 
 
         float maximum = index.model()->headerData(index.column(), Qt::Horizontal, 12).toFloat();
+        //dann zeichnen wir das Rechteck der Länge Datenwert/Maximum, um einen Faktor <= 1 zu erhalten, damit der Balken im Tabellenfeld gut dargestellt wird
+              rect.setWidth(rect.width()*(dataToFloat/maximum));
+              painter->drawRect(rect);}
 
-        if ((index.model()->headerData(index.column(),Qt::Horizontal)).toString() == "Start")
+       else if ((index.model()->headerData(index.column(),Qt::Horizontal)).toString() == "Start")
         {
             float s=(index.data().toFloat())/10;
             QRect rect = option.rect;
@@ -45,13 +49,6 @@ void bardelegatepep::paint(QPainter *painter, const QStyleOptionViewItem & optio
             painter->drawRect(rect);
         }
 
-        else{
-
-
-  //dann zeichnen wir das Rechteck der Länge Datenwert/Maximum, um einen Faktor <= 1 zu erhalten, damit der Balken im Tabellenfeld gut dargestellt wird
-        rect.setWidth(rect.width()*(dataToFloat/maximum));
-        painter->drawRect(rect);}
-    }
 
 
     else
